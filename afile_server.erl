58ee813efb5328cf233416d510518934691be1cf
Colin Detwiller <colinsr@me.com>
2016-02-03 07:47:30 -0500

    fix syntax

--- afile_server.erl
+++ afile_server.erl
@@ -8,7 +8,7 @@ loop(Dir) ->
     {Client, list_dir} ->
       Client ! {self(), file:list_dir(Dir)};
     {Client, {get_file, File}} ->
-      Full = filename:join(Dir,File).
+      Full = filename:join(Dir,File),
       Client ! {self(), file:read_file(Full)}
-  end
+  end,
   loop(Dir).
